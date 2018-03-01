8a492ebb098bf1ae8b453220ab850493446f7372
Seyhak Ly <seyhak.ly@gmail.com>
2018-03-01 10:59:44 +0100

    Mały błąd - brak connection.Close

--- WokItEasy/Menu.cs
+++ WokItEasy/Menu.cs
@@ -26,6 +26,7 @@ namespace WokItEasy
             DataSet data = new DataSet();
             AdapterTabela1.Fill(data, "Kategoria");
             a= data.Tables["Kategoria"].Rows[0][0].ToString();
+            connection.Close();
             return a;
         }
         public void Pokaż()
